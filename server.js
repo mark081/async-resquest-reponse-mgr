232178a60654361e8047a691fba92bd57eb38716
Mark Cooper <mcooper@citrine.io>
2020-01-21 15:24:02 -0800

    Added 0.0.0.0 specifically

--- server.js
+++ server.js
@@ -8,11 +8,11 @@ fastify.get('/', async (request, reply) => {
 // Run the server!
 const start = async () => {
   try {
-    await fastify.listen(8080);
+    await fastify.listen(8080, '0.0.0.0');
     fastify.log.info(`server listening on ${fastify.server.address().port}`)
   } catch (err) {
     fastify.log.error(err);
-    process.exit(1)
+    process.exit(1);
   }
 };
 start();
